c504c6afced789e507eec51c7a65f9741b3503d1
Arjun SK <arjunsk15@gmail.com>
2018-02-04 23:36:20 +0530

    backend first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = express();
+
+var http = require("http"); // for fetching txt file
+
+var port = process.env.PORT || config.PORT; // HEROKU requirement
+
+// split words by space
+function splitByWords (text) {
+    var wordsArray = text.split(/\s+/);
+    return wordsArray;
+}
+  
+  function createWordMap (wordsArray) {
+    var wordsMap = {};
+    wordsArray.forEach(function (key) {
+      if (wordsMap.hasOwnProperty(key)) {
+        wordsMap[key]++;
+      } else {
+        wordsMap[key] = 1;
+      }
+    });
+    return wordsMap;
+  }
+  
+  function sortByCount (wordsMap) {
+    var finalWordsArray = [];
+    finalWordsArray = Object.keys(wordsMap).map(function(key) {
+      return {
+        value: key,
+        count: wordsMap[key]
+      };
+    });
+  
+    finalWordsArray.sort(function(a, b) {
+      return b.count -  a.count;
+    });
+  
+    return finalWordsArray;
+  
+  }
+  
+app.get('/:count', function (req, res) {
+
+    let count = req.params.count;
+
+    http.get('http://terriblytinytales.com/test.txt').on('response', function (response) {
+        
+        var body = '';
+        var i = 0;
+        
+        //streaming the data in chunks
+        response.on('data', function (chunk) {
+            i++;
+            body += chunk;
+        });
+
+        //when done fetching
+        response.on('end', function () {
+    
+            var wordsArray = splitByWords(body);
+            var wordsMap = createWordMap(wordsArray);
+            var finalWordsArray = sortByCount(wordsMap);
+
+            var topNwords = finalWordsArray.slice(0, count);
+
+            res.send(topNwords); //creates an API
+            
+        });
+    });
+    
+
+});
+
+app.listen(port, function () {
+  console.log('Server running on'+ port);
+});
